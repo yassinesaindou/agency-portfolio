b0130e972bb18a8331a8516da63aee9196cd0939
Yassine Saindou <yassinesaindou12@gmail.com>
2024-11-26 23:41:20 +0530

    Build Contact Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
-import Services from "./sections/Services";
+import Contact from "./sections/Contact";
 
 export default function Home() {
   return <div className=" max-w-[1140px] mx-auto">
-    <Services />
+    <Contact />
   </div>;
 }
